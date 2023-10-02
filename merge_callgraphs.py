ed534fedf54454e103c98000c29b842e4ead47df
LittleLerry <76429680+LittleLerry@users.noreply.github.com>
2023-10-02 11:37:31 +0800

    Update merge_callgraphs.py

--- merge_callgraphs.py
+++ merge_callgraphs.py
@@ -3,6 +3,7 @@
 import argparse
 import networkx as nx
 
+#* $AFLGO/distance/distance_calculator/merge_callgraphs.py -o callgraph.dot $(ls callgraph.*)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -11,10 +12,13 @@ def main():
 
     args = parser.parse_args()
 
+    #* callgraph.xxx.dot
+
     G = nx.DiGraph()
     for dot in args.dot:
         G.update(nx.DiGraph(nx.drawing.nx_pydot.read_dot(dot)))
 
+    #* output files
     with open(args.out, 'w') as f:
         nx.drawing.nx_pydot.write_dot(G, f)
 
